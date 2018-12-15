413afe402fe92bedbb81c582c1089b71bdf6017b
Ilya <rickmortyand4@gmail.com>
2018-12-15 18:22:59 +0300

    add services, team_quotes; change js

--- src/js/bouncy_script.js
+++ src/js/bouncy_script.js
@@ -1,34 +1,10 @@
 'use strict'
+$(window).ready(function(){
+    $("#a1").addClass("begin_style");
 
-var numberImage1 = 1;
-var numberImage2 = 1;
-setInterval(
-    function(){
-        if ( numberImage1 == 1 ){
-            document.getElementById("work1").style.backgroundImage="url(" + 
-            "'../resources/img/featured_work/featured_work2_2.jpg')";
-            numberImage1++
-        }
-        else{
-            document.getElementById("work1").style.backgroundImage="url(" + 
-            "'../resources/img/featured_work/featured_work2_1.jpg')";
-            numberImage1--;
-        }
-    },
-8000);
-
-
-setInterval(
-    function(){
-        if ( numberImage2 == 1 ){
-            document.getElementById("work2").style.backgroundImage="url(" + 
-            "'../resources/img/featured_work/featured_work3_2.jpg')";
-            numberImage2++
-        }
-        else{
-            document.getElementById("work2").style.backgroundImage="url(" + 
-            "'../resources/img/featured_work/featured_work3_1.jpg')";
-            numberImage2--;
-        }
-    },
-6000);
+    if( $("#a1").attr("class") ){
+        $("#a2").click(function(){$("#a1").removeClass("begin_style");});
+        $("#a3").click(function(){$("#a1").removeClass("begin_style");});
+        $("#a4").click(function(){$("#a1").removeClass("begin_style");});
+    }
+});
